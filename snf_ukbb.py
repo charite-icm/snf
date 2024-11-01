2b87d48752d37a054cafed055a26bdac12b075a2
jakobVer <jakob.versnjak@gmail.com>
2024-11-01 10:14:00 +0100

    add snf_ukbb.py, used only for develop

---
+++ snf_ukbb.py
@@ -0,0 +1,35 @@
+import os 
+
+
+from src.snf import Snf, DistanceMetric
+
+
+DATA_PATH = "data/hfmodelexport_metab_prot_img_05_15_2024"
+MOD_DIRS = ("lab", "metabolomics_marcus_90", "physiology", "proteomics_all")
+# MOD_DIRS = ("lab", "metabolomics_marcus_90", "physiology")
+FEATHER_NAME = "ever_hfpef_suspect_noimg.feather"
+
+
+def main() -> None:
+    paths = [os.path.join(DATA_PATH, mod_dir, FEATHER_NAME) for mod_dir in MOD_DIRS]
+    mod_names = ("lab", "met", "phe", "pro")
+    save_path = "results/test_revised"
+
+    snf = Snf(data_paths=paths, mod_names=mod_names, save_path=save_path,
+              plot_missing_percentage=True, th_nan=0.3, random_state=41,
+              n_clusters=6, verbose=True)
+    
+
+    snf.set_affinity_matrix_param(K=0.1, mu=0.5, metric=DistanceMetric.SEUCLIDEAN, normalize=False)
+    snf.set_iterative_algorithm_param(t=20, alpha=1.0, top_k=20, edge_th=1.1)
+
+    snf.main()
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
